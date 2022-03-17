9ccb3a1d5f7bcf3f71ac08ad9e479d92f3d24525
p22jdiao <p22jdiao@fairfieldprep.org>
2022-03-17 10:05:20 -0400

    finished

---
+++ lab_14-1.py
@@ -0,0 +1,16 @@
+# Author: JD 03/17/2022
+import turtle
+window = turtle.Screen()
+window.setup(1000, 1000)
+window.screensize(800, 800)
+window.title("Lab 1")
+
+turtle1 = turtle.Turtle()
+turtle1.forward(250)
+turtle1.right(90)
+turtle1.forward(100)
+turtle1.right(90)
+turtle1.forward(250)
+turtle1.right(90)
+turtle1.forward(100)
+window.mainloop()
---
+++ lab_14-2.py
@@ -0,0 +1,16 @@
+# Author: JD 03/17/2022
+import turtle
+window = turtle.Screen()
+window.setup(500, 500)
+window.screensize(400, 400)
+window.title("Lab 2")
+
+turtle2 = turtle.Turtle()
+turtle2.right(60)
+turtle2.forward(100)
+turtle2.right(120)
+turtle2.forward(100)
+turtle2.right(120)
+turtle2.forward(100)
+
+window.mainloop()
